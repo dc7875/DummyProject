98eb066a8e2d4bc98f667c97b89c404eeccc17be
Durgesh094 <durgeshchavan1994@gmail.com>
2021-12-14 18:49:03 +0530

    Substraction Operation

--- src/com/project/DummyProgram.java
+++ src/com/project/DummyProgram.java
@@ -4,6 +4,18 @@ public class DummyProgram {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, I am Durgesh");
+		
+		// Addition 
+		int x = 2;
+		int y = 2;
+		int z = x+y;
+		System.out.println(z);
+		
+		// Subtraction
+		int a = 4;
+		int b = 2;
+		int c = a-b;
+		System.out.println(c);
 
 	}
 
