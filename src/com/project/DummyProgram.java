f948152819f0559b24613717ea89d99bf02da5f4
Durgesh094 <durgeshchavan1994@gmail.com>
2021-12-14 18:40:33 +0530

    Addition Operation

--- src/com/project/DummyProgram.java
+++ src/com/project/DummyProgram.java
@@ -4,6 +4,10 @@ public class DummyProgram {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, I am Durgesh");
+		int x = 2;
+		int y = 2;
+		int z = x+y;
+		System.out.println(z);
 
 	}
 
